b5ece83a0e87c3ac4ffa6ba712a9a91533b1998a
winterroad <winterroaduk@gmail.com>
2018-03-11 02:40:49 +0200

    Style/docs: added/edited comments

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const allPictures = ["<img class='unselected' src='img/no1.svg' alt='Number 1'>"
 "<img class='unselected' src='img/no12.svg' alt='Number 12'>", "<img class='unselected' src='img/no13.svg' alt='Number 13'>", "<img class='unselected'src='img/no14.svg' alt='Number 14'>", "<img class='unselected'src='img/no15.svg' alt='Number 15'>",
 "<img class='unselected' src='img/no16.svg' alt='Number 16'>"];
 const allPicturesNum = allPictures.length;
-let cardsInGame = 8; //how many different cards . CardsInGame x 2 x <div class="card or class="card"> (Add randomized src + alt here, from chosenPictures Array)<div>
+let cardsInGame = 8; //how many different cards . CardsInGame x 2 x <div class="card> (Add randomized src + alt here, from chosenPictures Array)<div>
 let chosenPictures = []; //Randomize pictures and add them in this Array.
 let pairsFound = 0; //In the beginning no pairs found.
 let stars = 3; //Max stars in the beginning.
@@ -17,35 +17,47 @@ let moves = 0; //Move = 2 cards "turned".
 
 //Lets select the pictures (1 each). After this these are added to game in randomized order (2 each). -> function createGame().
 chosenPictures = generateRandomPictures(cardsInGame);
+
+//Create a game based  on these pictures.
 createGame(chosenPictures);
 
+//Function for creating a game.
 function createGame(chosenPictures) {
 
+  //Lets add array that has pictures once to itself, a new array has been formed. Old arrays not affected.
   let allCards = chosenPictures.concat(chosenPictures);
+  //We need all cards array's size for for-loop.
   let allCardsNum = allCards.length;
-  console.log("chosenPictures: " + chosenPictures);
-  console.log("AllCards: " + allCards);
 
+  //To prevent reflow and repaint, lets create a document fragment. https://developer.mozilla.org/en-US/docs/Web/API/Document/createDocumentFragment
   const fragment = document.createDocumentFragment();
 
+  //For loop that goes as many "rounds" as there is elements in the array.
   for(let i=0; i < allCardsNum; i++){
 
-    allCardsSize = allCards.length;
+    //As the size of the array will change we have to check the size of the array, so we do not pick a index that is out of bounds.
+    let allCardsSize = allCards.length;
     //DOTO: all use randomize function that use for all randomizing needs.
+    //Lets randomize an index.
     index = Math.floor(Math.random()*allCardsSize);
-    element = allCards.splice(index, 1);
+    //Lets remove the element with previous index, save it variable element.
+    let element = allCards.splice(index, 1);
+    //Create a div that will be the card.
     let elementToBeAdded = document.createElement("div");
-    if (elementToBeAdded.classList){
+    //Add a card - class, with this should work even with IE 8.
+    if (elementToBeAdded.classList) {
       elementToBeAdded.classList.add("card");
     } else {
         elementToBeAdded.className += ' ' + "card";
-  }
+      }
+    //Add img info inside div.
     elementToBeAdded.innerHTML = element[0][0];
-    console.log(element[0][0]);
-    console.log(elementToBeAdded);
+
+    //Add card div with img info to document fragment.
     fragment.appendChild(elementToBeAdded);
   }
 
+  //Add the document fragment that has all the divs with pic-info to the game-div.
   game.appendChild(fragment);
 }
 
